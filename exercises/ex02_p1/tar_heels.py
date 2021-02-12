b7e6eefc1915c2d592b58fd1698c24a76b16f042
Peyton Wisniewski <peytonlw@live.unc.edu>
2021-02-12 18:28:02 -0500

    th 2.0 done

--- exercises/ex02_p1/tar_heels.py
+++ exercises/ex02_p1/tar_heels.py
@@ -1,16 +1,32 @@
 """Tar Heels exercise redux as a structured program."""
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730246281"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: int = int(input("Enter an int: "))
-    # TODO 2: Print the response of calling the tar_heels function here.
+    print(tar_heels(choice))
 
 
-# TODO 1: Define the tar_heels function, and its logic, here.
+def tar_heels(g: int) -> str:
+    a: int = g
+    b: int = a % 2
+    c: int = a % 7
+    d: bool = b == 0 and c == 0
+    e: bool = b == 0
+    f: bool = c == 0
 
+    if d is True: 
+        return("TAR HEELS")
+    else:
+        if f is True:
+            return("HEELS")
+        else:
+            if e is True:
+                    return("TAR")
+            else:
+                return("CAROLINA")
 
 if __name__ == "__main__":
     main()
